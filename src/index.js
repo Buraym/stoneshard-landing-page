7997506fee6ddca84dd9319e92738265df893784
EasyPeasy <BrayanWilis@outlook.com>
2020-12-27 09:58:31 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/App.js';
-import reportWebVitals from '../src/reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
